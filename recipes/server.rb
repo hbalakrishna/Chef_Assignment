4f4c8c364259a1d7b62a77d6198e2773b69312ae
Cloud User <centos@ip-172-31-95-231.ec2.internal>
2018-03-30 17:02:29 +0000

    Apache: Ohia Assignment

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,9 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do 
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world! </h1>
+          <h2>IPADDRESS: #{node['ipaddress']}</h2>
+          <h2>HostName: #{node['hostname']}</h2>"
 end
 
 service 'httpd' do
